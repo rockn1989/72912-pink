01333519726f0c6f1dc1ec2403615ca331d7245b
Artem Ryabkov <gold_100@bk.ru>
2016-06-20 23:13:02 +0300

    Флексбокс сетка

--- js/main.js
+++ js/main.js
@@ -26,3 +26,41 @@
   openMenu.addEventListener('click', menuToggle);
 
 })();
+
+/*
+(function() {
+
+  var reviewControl = {
+    leftArrow : document.querySelector('.review-slider__left-arrow'),
+    rightArrow: document.querySelector('.review-slider__left-arrow'),
+    sliderWrapper: document.querySelector('.review-slider__wrapper'),
+    slidesCount: document.querySelectorAll('.review-slider__slide'),
+    slideMargin: parseInt(getComputedStyle(document.querySelector('.review-slider__slide')).marginRight),
+    slideWidth: document.querySelector('.review-slider__slide').clientWidth,
+    count: 0
+  }
+
+
+  reviewControl.leftArrow.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    reviewControl.count--;
+
+    if(reviewControl.count <= 0) {
+
+      reviewControl.sliderWrapper.style.left += (reviewControl.slidesCount.length - 1 ) * (reviewControl.slideWidth + reviewControl.slideMargin) * -1 + 'px';
+
+      reviewControl.count = reviewControl.slidesCount.length - 1;
+
+      console.log(reviewControl.count)
+
+    } else {
+      reviewControl.sliderWrapper.style.left =  ((reviewControl.slideWidth + reviewControl.slideMargin) * reviewControl.count) * -1  + 'px';
+
+      console.log(reviewControl.count)
+    }
+
+  });
+
+})();
+*/
